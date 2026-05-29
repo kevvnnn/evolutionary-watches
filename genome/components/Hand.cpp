c447c7097f877413aab5142e6fd8013c30cf452d
Keane M A <keanemab@gmail.com>
2026-05-29 23:36:29 +0800

    Finished Hand.h and .cpp file

--- genome/components/Hand.cpp
+++ genome/components/Hand.cpp
@@ -1,1 +1,23 @@
+#include "Hand.h"
 
+Hand::Hand(unsigned int id, HandType type, double length, double balance, double weight, double baseFriction)
+    : WatchComponent(id, HAND, baseFriction), m_type(type),
+    m_length(length), m_balance(balance), m_weight(weight) {
+
+    }
+
+double Hand::calculateEfficiency() const {
+    return m_balance * (1.0 - (m_weight * m_length / 1000.0));
+}
+
+double Hand::calculateActualFriction() const {
+    return getBaseFriction();
+}
+
+std::string Hand::getTypeName() const {
+    return "Hand";
+}
+
+HandType Hand::getHandType() const {
+    return m_type;
+}
--- genome/components/Hand.h
+++ genome/components/Hand.h
@@ -1,1 +1,28 @@
+#ifndef HAND_H
+#define HAND_H
 
+#include "../WatchComponent.h"
+
+enum class HandType {
+    HOUR,
+    MINUTE,
+    SECOND
+};
+
+class Hand : public WatchComponent {
+    private:
+    HandType m_type;
+    double m_length;
+    double m_balance;
+    double m_weight;
+
+    public:
+    Hand(unsigned int id, HandType type, double length, double balance, double weight = 0.5, double baseFriction = 0.05);
+    double calculateEfficiency() const override;
+    double calculateActualFriction() const override;
+    std::string getTypeName() const override;
+
+    HandType getHandType() const;
+};
+
+#endif
