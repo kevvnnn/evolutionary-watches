0a50afd65434733f2c843ef70f14146a48169365
Keane M A <keanemab@gmail.com>
2026-05-30 00:26:14 +0800

    Did Watch.h file

---
+++ genome/Watch.h
@@ -0,0 +1,23 @@
+#include <vector>
+#include <map>
+
+#include "WatchComponent.h"
+
+class Watch {
+    private:
+    std::vector<WatchComponent*> m_components;
+    std::map<unsigned int, std::vector<unsigned int>> m_connections;
+
+    WatchComponent* findConponentByID(unsigned int id) const;
+
+    public:
+    Watch() = default;
+    ~Watch();
+
+    void addComponent(WatchComponent* component);
+    void connect(unsigned int idA, unsigned int idB);
+    bool isValid() const;
+    const std::vector<WatchComponent*>& getComponents() const;
+
+    void updateJewelPlacementFriction();
+};
