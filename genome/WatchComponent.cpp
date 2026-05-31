5efdd512edc1b8c93ba9f18f4987cafef63095c0
Keane M A <keanemab@gmail.com>
2026-05-31 23:41:08 +0800

    Fixed WatchCOmponent.cpp to matchthe header

--- genome/WatchComponent.cpp
+++ genome/WatchComponent.cpp
@@ -1,22 +1,74 @@
 #include "WatchComponent.h"
+#include <stdexcept>
 
-WatchComponent::WatchComponent(unsigned int id, ComponentType type, double baseFriction)
- : m_componentID(id), m_componentType(type), m_baseFriction(baseFriction) {
+using namespace WatchGA::Genome;
 
+WatchComponent::WatchComponent()
+    : m_weight(1.0),
+      m_friction(0.0),
+      m_x(0.0),
+      m_y(0.0),
+      m_rotation(0.0)
+{
 }
 
-WatchComponent::~WatchComponent () {
-
+WatchComponent::WatchComponent(const std::string& name, double weight, double friction, double x, double y)
+    : Core::SystemObject(name),
+      m_weight(weight),
+      m_friction(friction),
+      m_x(x),
+      m_y(y),
+      m_rotation(0.0)
+{
+    SetWeight(weight);
+    SetFriction(friction);
 }
 
-unsigned int WatchComponent::getID() const {
-    return m_componentID;
+double WatchComponent::GetWeight() const {
+    return m_weight;
 }
 
-ComponentType WatchComponent::getComponentType() const {
-    return m_componentType;
+double WatchComponent::GetFriction() const {
+    return m_friction;
 }
 
-double WatchComponent::getBaseFriction() const {
-    return m_baseFriction;
+double WatchComponent::GetX() const {
+    return m_x;
+}
+
+double WatchComponent::GetY() const {
+    return m_y;
+}
+
+double WatchComponent::GetRotation() const {
+    return m_rotation;
+}
+
+void WatchComponent::SetWeight(double weight) {
+    if (weight <= 0.0)
+        throw std::invalid_argument("Weight must be positive.");
+    m_weight = weight;
+}
+
+void WatchComponent::SetFriction(double friction) {
+    if (friction < 0.0) m_friction = 0.0;
+    else if (friction > 1.0) m_friction = 1.0;
+    else m_friction = friction;
+}
+
+void WatchComponent::SetPosition(double x, double y) {
+    m_x = x;
+    m_y = y;
+}
+
+void WatchComponent::SetRotation(double rotation) {
+    m_rotation = rotation;
+    while (m_rotation >= 360.0) m_rotation -= 360.0;
+    while (m_rotation < 0.0) m_rotation += 360.0;
+}
+
+std::string WatchComponent::ToString() const {
+    return Core::SystemObject::ToString() +
+           " [Weight: " + std::to_string(m_weight) +
+           "g, Friction: " + std::to_string(m_friction) + "]";
 }
