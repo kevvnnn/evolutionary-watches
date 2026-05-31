4d913193d537e382a48b61be50179d6f7b2f45cb
Keane M A <keanemab@gmail.com>
2026-06-01 00:01:24 +0800

    Fixed Jewel.cpp to match header

--- genome/components/Jewel.cpp
+++ genome/components/Jewel.cpp
@@ -1,38 +1,50 @@
 #include "Jewel.h"
 
-const double MIN_REQUIRED_FRICTION_FOR_JEWEL = 0.25;
+using namespace WatchGA::Genome;
+using namespace WatchGA::Genome::Components;
 
-Jewel::Jewel(unsigned int id, double hardness, bool isCapJewel, double baseFriction)
-    : WatchComponent(id, JEWEL, baseFriction), m_hardness(hardness), m_isCapJewel(isCapJewel), m_connectedComponentFriction(0.0) {
-
-    }
-
-double Jewel::calculateEfficiency() const {
-    double capBonus = m_isCapJewel ? 1.2 : 1.0;
-    return (m_hardness / 10.0) * capBonus;
+Jewel::Jewel()
+    : m_hardness(9.0),  // Sapphire/ruby hardness
+      m_isCapJewel(false)
+{
 }
 
-double Jewel::calculateActualFriction() const {
-    double frictionReduction = (m_hardness / 10.0) * 0.8;
-    return getBaseFriction() * (1.0 - frictionReduction);
+Jewel::Jewel(const std::string& name, double weight, double friction, double x, double y,
+             double hardness, bool isCapJewel)
+    : WatchComponent(name, weight, friction, x, y),
+      m_hardness(hardness),
+      m_isCapJewel(isCapJewel)
+{
+    SetHardness(hardness);
 }
 
-std::string Jewel::getTypeName() const {
-    return "Jewel";
+// Getters
+double Jewel::GetHardness() const { return m_hardness; }
+bool Jewel::IsCapJewel() const { return m_isCapJewel; }
+
+// Hardness clamped 0–10 (Mohs scale)
+void Jewel::SetHardness(double hardness) {
+    if (hardness < 0.0) m_hardness = 0.0;
+    else if (hardness > 10.0) m_hardness = 10.0;
+    else m_hardness = hardness;
 }
 
-void Jewel:: setConnectedFriction(double friction) {
-    m_connectedComponentFriction = friction;
+void Jewel::SetIsCapJewel(bool isCapJewel) {
+    m_isCapJewel = isCapJewel;
 }
 
-bool Jewel::isPlacedCorrectly() const {
-    return m_connectedComponentFriction >= MIN_REQUIRED_FRICTION_FOR_JEWEL;
+// Jewel efficiency = hardness + cap bonus
+double Jewel::CalculateEfficiency() const {
+    return (m_hardness / 10.0) * (m_isCapJewel ? 1.2 : 1.0);
 }
 
-double Jewel::getPlacementBonus() const {
-    return isPlacedCorrectly() ? 0.15 : 0.0;
+// Deep copy
+std::unique_ptr<WatchComponent> Jewel::Clone() const {
+    return std::make_unique<Jewel>(*this);
 }
 
-double Jewel::getPlacementPenalty() const {
-    return isPlacedCorrectly() ? 0.0 : 0.25;
+std::string Jewel::ToString() const {
+    return WatchComponent::ToString() +
+           " [Jewel | Hardness: " + std::to_string(m_hardness) +
+           ", Cap: " + (m_isCapJewel ? "Yes" : "No") + "]";
 }
