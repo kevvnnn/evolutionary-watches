f936079016fc779438b967de6b32224aa17ae8d4
Keane M A <keanemab@gmail.com>
2026-05-31 23:46:44 +0800

    Fixed GEar.cpp to match header file

--- genome/components/Gear.cpp
+++ genome/components/Gear.cpp
@@ -1,22 +1,69 @@
 #include "Gear.h"
+#include <stdexcept>
 
-Gear::Gear(unsigned int id, unsigned int teeth, double diameter, double meshQuality, double baseFriction)
-    : WatchComponent(id, GEAR, baseFriction), m_toothCount(teeth), m_diameter(diameter), m_meshingQuality(meshQuality) {
+using namespace WatchGA::Genome;
+using namespace WatchGA::Genome::Components;
 
-    }
+// Default gear with reasonable defaults
+Gear::Gear()
+    : m_toothCount(10),
+      m_diameter(5.0),
+      m_meshingQuality(0.8)
+{
+}
 
-double Gear::calculateEfficiency() const {
+// Full constructor with validation
+Gear::Gear(const std::string& name, double weight, double friction, double x, double y,
+           unsigned int toothCount, double diameter, double meshingQuality)
+    : WatchComponent(name, weight, friction, x, y),
+      m_toothCount(toothCount),
+      m_diameter(diameter),
+      m_meshingQuality(meshingQuality)
+{
+    SetToothCount(toothCount);
+    SetDiameter(diameter);
+    SetMeshingQuality(meshingQuality);
+}
+
+// Getters
+unsigned int Gear::GetToothCount() const { return m_toothCount; }
+double Gear::GetDiameter() const { return m_diameter; }
+double Gear::GetMeshingQuality() const { return m_meshingQuality; }
+
+// ------------------------------
+// Validation
+// ------------------------------
+void Gear::SetToothCount(unsigned int toothCount) {
+    if (toothCount < 8)
+        throw std::invalid_argument("Tooth count minimum is 8.");
+    m_toothCount = toothCount;
+}
+
+void Gear::SetDiameter(double diameter) {
+    if (diameter < 1.0)
+        throw std::invalid_argument("Diameter minimum is 1mm.");
+    m_diameter = diameter;
+}
+
+void Gear::SetMeshingQuality(double quality) {
+    if (quality < 0.0) m_meshingQuality = 0.0;
+    else if (quality > 1.0) m_meshingQuality = 1.0;
+    else m_meshingQuality = quality;
+}
+
+// Gear efficiency formula
+double Gear::CalculateEfficiency() const {
     return m_meshingQuality * (1.0 - (1.0 / m_toothCount)) * (1.0 - (m_diameter / 100.0));
 }
 
-double Gear::calculateActualFriction() const {
-    return getBaseFriction();
+// Create deep copy for GA
+std::unique_ptr<WatchComponent> Gear::Clone() const {
+    return std::make_unique<Gear>(*this);
 }
 
-std::string Gear::getTypeName() const {
-    return "Gear";
-}
-
-unsigned int Gear ::getToothCount() const {
-    return m_toothCount;
+// Debug string
+std::string Gear::ToString() const {
+    return WatchComponent::ToString() +
+           " [Gear | Teeth: " + std::to_string(m_toothCount) +
+           ", Diameter: " + std::to_string(m_diameter) + "mm]";
 }
