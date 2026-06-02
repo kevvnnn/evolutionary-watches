9596f97f02af9a20a68c41eb05615adfc3aed4ea
Keane M A <keanemab@gmail.com>
2026-06-02 17:03:15 +0800

    Added comments to Jewel.cpp. not 67 lines of code this time

--- genome/components/Jewel.cpp
+++ genome/components/Jewel.cpp
@@ -4,7 +4,7 @@ using namespace WatchGA::Genome;
 using namespace WatchGA::Genome::Components;
 
 Jewel::Jewel()
-    : m_hardness(9.0),
+    : m_hardness(9.0),  // Real life sapphire/ruby hardness
       m_isCapJewel(false)
 {
 }
@@ -18,9 +18,11 @@ Jewel::Jewel(const std::string& name, double weight, double friction, double x, 
     setHardness(hardness);
 }
 
+// Getters
 double Jewel::getHardness() const { return m_hardness; }
 bool Jewel::isCapJewel() const { return m_isCapJewel; }
 
+// Hardness clamped 0-10 (Mohs scale)
 void Jewel::setHardness(double hardness) {
     if (hardness < 0.0) m_hardness = 0.0;
     else if (hardness > 10.0) m_hardness = 10.0;
@@ -31,10 +33,12 @@ void Jewel::setIsCapJewel(bool isCapJewel) {
     m_isCapJewel = isCapJewel;
 }
 
+// Jewel efficiency = hardness + cap bonus
 double Jewel::calculateEfficiency() const {
     return (m_hardness / 10.0) * (m_isCapJewel ? 1.2 : 1.0);
 }
 
+// Deep copy
 std::unique_ptr<WatchComponent> Jewel::clone() const {
     return std::make_unique<Jewel>(*this);
 }
